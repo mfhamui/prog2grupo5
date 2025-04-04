443ff339114924876440249a96370407c510b132
margaritasouviron <msouviron@udesa.edu.ar>
2025-04-04 12:13:49 -0300

    las rutas

---
+++ myapp/routes/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router;
+
+
+router.get('/', );
+router.get('/index', );
+router.get('/login', );
+router.get('/product-add', );
+router.get('/product', );
+router.get('/register', );
+router.get('/search-result', );
