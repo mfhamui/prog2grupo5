f369169dd3c62b8cdf680f61847b5c44c3a0b09c
margaritasouviron <msouviron@udesa.edu.ar>
2025-04-14 10:48:46 -0300

    cambio en las rutas

--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -9,6 +9,6 @@ router.get('/profile', productController.profile);
 router.get('/product-add', productController.product_add );
 router.get('/product', productController.product);
 router.get('/register', productController.register);
-router.get('/search-results', productController.search_result);
+router.get('/search-results/:search?', productController.search_result);
 
 module.exports = router;
