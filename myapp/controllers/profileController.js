e7aef570830acb39ee9b2427541d8dd79cf3cbe1
margaritasouviron <msouviron@udesa.edu.ar>
2025-05-26 09:53:00 -0300

    errores

--- myapp/controllers/profileController.js
+++ myapp/controllers/profileController.js
@@ -3,37 +3,29 @@ const db = require('../database/models')
 const User = db.User;
 const bcrypt = require("bcryptjs");
 
- let profileController = {
-    login: function(req,res){
-        
-        let datos = req.body;
-        db.User.findAll({
-            where: [{email: datos.email}]
-        })
-        .then(function(results){
-            if (results != undefined) {
-                return res.render('login', {datos: "debe ingresar otro email"})
-            }
-        })
-        
+let profileController = {
+login: function(req,res){
+    
+    return res.render('login')
+    
+    
 
-    },
-    register: function(req,res){
-        
-        return res.render('register')
-    }, 
-    profile: function(req,res){
-        return res.render('profile', {  nombre: data.usuario.nombre,
-                                        email: data.usuario.email,
-                                        contrasenia: data.usuario.password,
-                                        nacimiento: data.usuario.fechaNacimiento,
-                                        documento: data.usuario.documento,
-                                        foto: data.usuario.foto,
-                                        productos: data.productos
-                                     })
-    },
+},
+register: function(req,res){
+    return res.render('register')
+}, 
+profile: function(req,res){
+    return res.render('profile', {  nombre: data.usuario.nombre,
+                                    email: data.usuario.email,
+                                    contrasenia: data.usuario.password,
+                                    nacimiento: data.usuario.fechaNacimiento,
+                                    documento: data.usuario.documento,
+                                    foto: data.usuario.foto,
+                                    productos: data.productos
+                                    })
+},
 
- };
+};
 
  module.exports = profileController;
 
