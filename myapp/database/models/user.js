5f60f0d0e522d490c90ac0c3c9ace93500b98d62
felih <hamuim@udesa.edu.ar>
2025-05-26 09:23:48 -0300

    modelo users

---
+++ myapp/database/models/user.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = 'Usuario'; // Nombre del modelo
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER.UNSIGNED
+        },
+        email: {
+            type: dataTypes.STRING(500),
+         
+        },
+        nombreUsuario: {
+            type: dataTypes.STRING(500),
+         
+            unique: true
+        },
+        contrasenia: {
+            type: dataTypes.STRING(500),
+           
+        },
+        fechaNacimiento: {
+            type: dataTypes.DATEONLY,
+           
+        },
+        documento: {
+            type: dataTypes.INTEGER.UNSIGNED,
+           
+        },
+        foto: {
+            type: dataTypes.STRING(500),
+            
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updatedAt: {
+            type: dataTypes.DATE
+        },
+        deletedAt: {
+            type: dataTypes.DATE
+        }
+    };
+
+    let config = {
+        tableName: "usuario", 
+        timestamps: true,
+        paranoid: true,
+        underscored: false
+    };
+
+    const Usuario = sequelize.define(alias, cols, config);
+
+    return Usuario;
+};
